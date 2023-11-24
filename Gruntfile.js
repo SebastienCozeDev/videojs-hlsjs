da53cc96cba2d3dd94fbd6cebc6a76f8a0dc60c6
SebastienCozeDev <sebastien.coze.62@outlook.fr>
2023-11-24 09:45:39 +0100

    Update Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,70 +1,63 @@
-'use strict';
+/* global module, require */
 
-module.exports = function (grunt) {
-  require('load-grunt-tasks')(grunt);
+(function() {
+  'use strict';
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
-            '<%= grunt.template.today("yyyy-mm-dd") %>*/\n',
-    clean: {
-      files: ['dist']
-    },
-    connect: {
-      main: {
+  module.exports = function (grunt) {
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
+      banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+              '<%= grunt.template.today("yyyy-mm-dd") %>*/\n',
+      clean: {
+        files: ['dist']
+      },
+      connect: {
+        main: {
+          options: {
+            port: 9000,
+            protocol: 'http',
+            hostname: '*'
+          }
+        }
+      },
+      concat: {
         options: {
-          port: 9000,
-          protocol: 'http',
-          hostname: '*'
+          banner: '<%= banner %>',
+          stripBanners: true
+        },
+        dist: {
+          src: ['src/**/*.js'],
+          dest: 'dist/<%= pkg.name %>.js'
+        }
+      },
+      uglify: {
+        options: {
+          banner: '<%= banner %>',
+          mangle: {
+            reserved: ['Hlsjs']
+          }
+        },
+        dist: {
+          src: '<%= concat.dist.dest %>',
+          dest: 'dist/<%= pkg.name %>.min.js'
+        }
+      },
+      watch: {
+        gruntfile: {
+          files: '<%= jshint.gruntfile.src %>',
+          tasks: ['jshint:gruntfile']
+        },
+        src: {
+          files: '<%= jshint.src.src %>',
+          tasks: ['jshint:src']
         }
       }
-    },
-    concat: {
-      options: {
-        banner: '<%= banner %>',
-        stripBanners: true
-      },
-      dist: {
-        src: ['src/**/*.js'],
-        dest: 'dist/<%= pkg.name %>.js'
-      }
-    },
-    uglify: {
-      options: {
-        banner: '<%= banner %>'
-      },
-      dist: {
-        src: '<%= concat.dist.dest %>',
-        dest: 'dist/<%= pkg.name %>.min.js'
-      }
-    },
-    jshint: {
-      gruntfile: {
-        options: {
-          node: true
-        },
-        src: 'Gruntfile.js'
-      },
-      src: {
-        options: {
-          jshintrc: '.jshintrc'
-        },
-        src: ['src/**/*.js']
-      },
-    },
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      src: {
-        files: '<%= jshint.src.src %>',
-        tasks: ['jshint:src']
-      }
-    }
-  });
+    });
 
-  grunt.registerTask('build', ['jshint', 'concat', 'uglify']);
-  grunt.registerTask('serve', ['jshint', 'connect', 'watch']);
-  grunt.registerTask('default', ['clean', 'build']);
-};
+    grunt.registerTask('build', ['concat', /*'uglify'*/]);
+    grunt.registerTask('serve', ['connect', 'watch']);
+    grunt.registerTask('default', ['clean', 'build']);
+  };
+})();
